8ba018d0dfde1ce895f8f2904e789fb15aace98d
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2020-07-04 14:34:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 error_reporting(E_ALL & ~E_NOTICE);
 include 'vendor/autoload.php';
 include 'conststr.php';
-include 'common.php';
-
+include 'function.php';
+//require_once 'common.php';
+//$e['RUNTIME_USERDATA']['aaa']='bbb';
+//set_envs_to_ctx($e);
 //echo '<pre>'. json_encode($_SERVER, JSON_PRETTY_PRINT).'</pre>';
 if (isset($_SERVER['USER'])&&$_SERVER['USER']==='qcloud') {
     include 'platform/TencentSCF.php';
 } elseif (isset($_SERVER['FC_SERVER_PATH'])&&$_SERVER['FC_SERVER_PATH']==='/var/fc/runtime/php7.2') {
     //echo '<pre>'. json_encode($_SERVER, JSON_PRETTY_PRINT).'</pre>';
     include 'platform/AliyunFC.php';
+} elseif ($_SERVER['_APP_SHARE_DIR']=='/var/share/CFF/processrouter') {
+    // huawei
+    include 'platform/HuaweiFG.php';
 } elseif (isset($_SERVER['HEROKU_APP_DIR'])&&$_SERVER['HEROKU_APP_DIR']==='/app') {
     include 'platform/Heroku.php';
     $path = getpath();
@@ -56,31 +61,63 @@ function main_handler($event, $context)
     return main($path);
 }
 
-// Aliyun FC
-function handler($request, $context)
+// Aliyun FC & Huawei FG
+function handler($event, $context)
 {
-    set_error_handler("myErrorHandler");
-    $event = array(
-        'method' => $request->getMethod(),
-        'clientIP' => $request->getAttribute("clientIP"),
-        'requestURI' => $request->getAttribute("requestURI"),
-        'path' => spurlencode($request->getAttribute("path"), '/'),
-        'queryString' => $request->getQueryParams(),
-        'headers' => $request->getHeaders(),
-        'body' => $request->getBody()->getContents(),
-    );
-    $context = json_decode(json_encode($context), true);
-    printInput($event, $context);
-    unset($_POST);
-    unset($_GET);
-    unset($_COOKIE);
-    unset($_SERVER);
-    GetGlobalVariable($event);
-    $path = GetPathSetting($event, $context);
+    if (isset($_SERVER['FC_SERVER_PATH'])&&$_SERVER['FC_SERVER_PATH']==='/var/fc/runtime/php7.2') {
+        set_error_handler("myErrorHandler");
+        $tmp = array(
+            'method' => $event->getMethod(),
+            'clientIP' => $event->getAttribute("clientIP"),
+            'eventURI' => $event->getAttribute("eventURI"),
+            'path' => spurlencode($event->getAttribute("path"), '/'),
+            'queryString' => $event->getQueryParams(),
+            'headers' => $event->getHeaders(),
+            'body' => $event->getBody()->getContents(),
+        );
+        $event = $tmp;
+        $context = json_decode(json_encode($context), true);
+        printInput($event, $context);
+        unset($_POST);
+        unset($_GET);
+        unset($_COOKIE);
+        unset($_SERVER);
+        GetGlobalVariable($event);
+        $path = GetPathSetting($event, $context);
 
-    $re = main($path);
+        $re = main($path);
 
-    return new RingCentral\Psr7\Response($re['statusCode'], $re['headers'], $re['body']);
+        return new RingCentral\Psr7\Response($re['statusCode'], $re['headers'], $re['body']);
+
+    } elseif ($_SERVER['_APP_SHARE_DIR']=='/var/share/CFF/processrouter') {
+        // huawei
+        //$html = '<pre>'. json_encode($_SERVER, JSON_PRETTY_PRINT).'</pre>';
+        //$html .= '<pre>'. json_encode($event, JSON_PRETTY_PRINT).'</pre>';
+        //$html .= '<pre>'. json_encode($tmp, JSON_PRETTY_PRINT).'</pre>';
+        //$e['RUNTIME_USERDATA']['aaa']='bbb';
+        //$html=set_envs_to_ctx($e);
+        //$html = file_get_contents('common.php');
+        //$html = 'urn:'.getenv('HW_urn');
+        //$html = file_exists(__DIR__.'/theme/classic.html');
+        //return output($html);
+        global $contextUserData;
+        $contextUserData = $context;
+        $event = json_decode(json_encode($event), true);
+        if ($event['isBase64Encoded']) $event['body'] = base64_decode($event['body']);
+        //$context = json_decode(json_encode($tmp), true);
+        printInput($event, $context);
+        unset($_POST);
+        unset($_GET);
+        unset($_COOKIE);
+        unset($_SERVER);
+        GetGlobalVariable($event);
+        //echo '<pre>'. json_encode($_COOKIE, JSON_PRETTY_PRINT).'</pre>';
+        $path = GetPathSetting($event, $context);
+
+        $tmp = main($path);
+        //error_log('结果'.$html.json_encode($tmp, JSON_PRETTY_PRINT));
+        return $tmp;
+    }
 }
 
 // used by Aliyun FC
@@ -105,6 +142,5 @@ function myErrorHandler($errno, $errstr, $errfile, $errline) {
         break;
     }
 
-    /* Don't execute PHP internal error handler */
     return true;
 }
